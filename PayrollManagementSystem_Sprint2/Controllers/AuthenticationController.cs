7f76a6b8d901b3f8e972a4c88b4cec71f07efc48
Ankit Ashok Kumar Singh <singhanki67@gmail.com>
2021-09-12 04:18:09 +0530

    Revert "AuthenticationWithoutlocalhostLocation"
    This reverts commit 17795a9a41fff3ea05de8a517fc302d8a4e57d91.

--- PayrollManagementSystem_Sprint2/Controllers/AuthenticationController.cs
+++ PayrollManagementSystem_Sprint2/Controllers/AuthenticationController.cs
@@ -8,51 +8,19 @@ using Microsoft.AspNetCore.Identity;
 using System.Data.SqlClient;
 using System.Linq;
 using Microsoft.AspNetCore.Authorization;
-using PayrollManagementSystem_API.Models;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using Newtonsoft.Json;
-using Microsoft.AspNetCore.Http;
 
 namespace PayrollManagementSystem_Sprint2.Controllers
 {
 
     public class AuthenticationController : Controller
     {
-        private HttpContent content;
-
         public UserManager<IdentityUser> UserManager { get; }
         public SignInManager<IdentityUser> SignInManager { get; }
         public AuthenticationController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
-        } 
-        public async Task<EmployeeMaster>IsValidEmployee(AuthorizationResponse authResponse)
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("");
-                client.DefaultRequestHeaders.Clear();
-
-                //Defining Request Data Format
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                HttpResponseMessage Result = await client.PostAsync("employee/login", content);
-                if (Result.IsSuccessStatusCode)
-                {
-                    //storing the response details recieved from web api
-                    var employeeResponse = Result.Content.ReadAsStringAsync().Result;
-                    //Deserializing the response recieved from web Api and storing into the mployee list
-                    var resultValue = JsonConvert.DeserializeObject<LoginGet>(employeeResponse);
-                    HttpContext.Session.SetString("Token", resultValue._applicationSideToken);
-                    return resultValue.EmpMasterObj;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-        }
+        }        
         [HttpGet]
         public IActionResult Register()
         {
@@ -60,42 +28,9 @@ namespace PayrollManagementSystem_Sprint2.Controllers
         }
         
         
-        public ActionResult Login(AuthorizationResponse model)
+        public ActionResult Login()
         {
-            if (ModelState.IsValid)
-            {
-                //validation of the Employee weather the employee is valid or Not
-                var isValidUser = IsValidEmployee(model).Result;
-
-                //If the employee Is Valid and present in Db we redirect them to their respective Pages that is weather
-                //admin page or Employee Page
-                if (isValidUser != null)
-                {
-                    string isAdminString = null;
-                    if(isValidUser.AdminPrivilege == true)
-                    {
-                        isAdminString = "True";
-                    }
-                    else
-                    {
-                        isAdminString = "False";
-                    }
-                    HttpContext.Session.SetString("EmployeeID", isValidUser.EmployeeId);
-                    HttpContext.Session.SetString("EmpFirstName", isValidUser.EmployeeFirstname);
-                    HttpContext.Session.SetString("AdminPrivelege", isAdminString);
-                    return HttpContext.Session.GetString("AdminPrivelege") == "True" ? RedirectToAction("AdminController", "Index") : RedirectToAction("EmployeeController", "Index");
-                    //return View();
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "You entered wrong username and password Combination");
-                    return View();
-                }
-            }
-            else
-            {
-                return View(model);
-            }
+            return View();
         }
 
         
